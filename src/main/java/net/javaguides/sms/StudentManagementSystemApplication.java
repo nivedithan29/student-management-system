c6c2631188d3acfa8662731eed35988b3227360e
Niveditha N <163818215+nivedithan29@users.noreply.github.com>
2025-10-13 11:44:17 +0530

    initial commit

--- src/main/java/net/javaguides/sms/StudentManagementSystemApplication.java
+++ src/main/java/net/javaguides/sms/StudentManagementSystemApplication.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import net.javaguides.sms.entity.Student;
 import net.javaguides.sms.repository.StudentRepository;
 
@@ -36,3 +35,4 @@ public class StudentManagementSystemApplication implements CommandLineRunner{
 
 
 }
+
