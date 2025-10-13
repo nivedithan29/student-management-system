6f5c46ccb529b665adca3f633a4f12bcd19c5f7f
Niveditha N <163818215+nivedithan29@users.noreply.github.com>
2025-10-13 11:42:59 +0530

    update student.java

--- src/main/java/net/javaguides/sms/controller/StudentController.java
+++ src/main/java/net/javaguides/sms/controller/StudentController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import net.javaguides.sms.entity.Student;
 import net.javaguides.sms.service.StudentService;
 
@@ -74,4 +73,5 @@ public class StudentController {
 		return "redirect:/students";
 	}
 	
+
 }
